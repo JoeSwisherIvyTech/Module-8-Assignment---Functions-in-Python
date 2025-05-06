f38a0e5975c52321b223aabec319b4ee846cda77
Joe Swisher <jswisher6@ivytech.edu>
2025-05-06 15:30:44 -0400

    Create Module 8 Assignment - Functions in Python - Joe Swisher.py

---
+++ Module 8 Assignment - Functions in Python - Joe Swisher.py
@@ -0,0 +1,15 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def greater_than(x, y):
+...     if x > y:
+...         return True
+...     else:
+...         return False
+... 
+...     
+>>> a = 2
+>>> b = 3
+>>> 
+>>> result = greater_than(a, b)
+>>> print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
+The statement 2 is greater than 3 is False
